46d05d059c237405749141a0eb8cf392f99a106a
Ben Klein <robobenklein@gmail.com>
2016-08-25 14:13:52 -0400

    Conditionals?

---
+++ conditionals-tests/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	char gender;
+    inf fcount = 0, mcount =0;
+    cout << "Enter gender: ";
+    cin >> gender;
+    if (gender == 'F')
+        fcount++;
+    else
+        mcount++;
+    
+    cout << "There are " << fcount << " females and " << mcount << " males."
+	return 0;
+}
